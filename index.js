a76f1d4a9cfe1b7a6258b8a0e98f72e64e0724fa
Ryan Liao <ianryanliao@gmail.com>
2020-05-03 02:12:24 -0700

    fixed unintended exception being thrown even during successful application runs

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+const debug = process.argv.includes('--debug');
+const log4js = require('log4js');
+const logger = log4js.getLogger();
+logger.level = debug ? 'debug' : 'info';
+
 const fs = require('fs');
 const secretsPath = './secrets.json';
 if (!fs.existsSync(secretsPath)) {
-  console.log("Error: secrets.json not found, exiting");
+  logger.error('secrets.json not found, exiting');
   return;
 }
 
@@ -9,34 +14,49 @@ const secrets = require(secretsPath);
 const username = secrets.username;
 const password = secrets.password;
 if (!username || !password) {
-  console.log("Error: username and/or password not found in secrets.json, exiting");
+  logger.error('Error: username and/or password not found in secrets.json, exiting');
   return;
 }
 
 const puppeteer = require('puppeteer');
-const headless = !process.argv.includes('--showBrowser');
 const waitOptions = {waitUntil: ['load', 'networkidle0']};
 (async () => {
-  const browser = await puppeteer.launch({headless: headless, /* slowMo: 250 */ });
+  logger.info('Starting application to restart extender');
+  const browser = await puppeteer.launch({
+    headless: !debug,
+    slowMo: debug ? 250 : 0
+  });
   const page = await browser.newPage();
   await page.setViewport({width: 1200, height: 800, deviceScaleFactor: 2}); // viewport size set to prevent mobile version
   await page.goto('http://mywifiext.local', waitOptions);
+
   await page.type('#userId', username)
   await page.type('#password', password)
   await Promise.all([
     page.waitForNavigation(waitOptions),
     page.click('#loginBt'), // triggers nagivation
   ]);
-  await page.click('#settingsSpan');
+  logger.debug('Logged in to extender web console');
+
+  await page.click('#settingsSpan'); // not in a promise because click does not trigger navigation
   await Promise.all([
     page.waitForNavigation(waitOptions),
     page.click('#otherSettings'), // triggers nagivation
   ]);
-  await page.click('#restartBt');
-  await Promise.all([
-    page.waitForNavigation(waitOptions),
-    page.click('#restartYesBt'), // triggers navigation
-  ]);
+  logger.debug('Navigated to other settings page');
+
+  await page.click('#restartBt'); // not in a promise because click does not trigger naviation
+  await page.click('#restartYesBt'); // triggers navigation
+  logger.debug('Confirmed yes to restart extender');
+
+  await page.close();
   await browser.close();
-})();
+  logger.debug('Closed puppeteer browser');
+})().catch( e => {
+  logger.error('Unexpected exception occurred (run with --debug for more detail): ' + e);
+  process.exit(1);
+}).finally(() => {
+  logger.info('Successfully started power cycle to restart the extender');
+  process.exit(0);
+});
 
